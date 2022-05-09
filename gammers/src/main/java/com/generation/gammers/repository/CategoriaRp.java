29cfc0933eed8c3e24dec0ef84884593d36911f1
Helga <onegra138@gmail.com>
2022-05-09 09:29:44 -0300

    Gammers

--- gammers/src/main/java/com/generation/gammers/repository/CategoriaRp.java
+++ gammers/src/main/java/com/generation/gammers/repository/CategoriaRp.java
@@ -3,9 +3,11 @@ package com.generation.gammers.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.gammers.model.CategoriaMod;
 
+@Repository
 public interface CategoriaRp extends JpaRepository<CategoriaMod, Long> {
 public List<CategoriaMod> findAllByNomeContainingIgnoreCase(String nome);
 }
--- gammers/src/main/java/com/generation/gammers/repository/ProdutosRp.java
+++ gammers/src/main/java/com/generation/gammers/repository/ProdutosRp.java
@@ -3,9 +3,11 @@ package com.generation.gammers.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.gammers.model.ProdutosMod;
 
+@Repository
 public interface ProdutosRp extends JpaRepository<ProdutosMod, Long> {
 public List<ProdutosMod> findAllByNomeContainingIgnoreCase(String nome);
 }
